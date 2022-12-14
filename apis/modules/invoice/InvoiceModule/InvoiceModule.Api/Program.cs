d4ef9db353882f65069960732f332c75f75c644e
Mattias Nordqvist <mattias.nordqvist@spinit.se>
2022-12-14 16:16:10 +0100

    added invoiceapi

---
+++ apis/modules/invoice/InvoiceModule/InvoiceModule.Api/Program.cs
@@ -0,0 +1,40 @@
+using InvoiceModule.Api.Controllers;
+
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration, "AzureAd");
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.OperationFilter<AuthorizeOperationFilter>();
+    c.AddSecurityDefinition("oauth2", new SecurityScheme(builder.Configuration));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(setup =>
+    {
+        setup.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+        setup.SwaggerEndpoint($"/swagger/v1/swagger.json", "Version 1.0");
+        setup.OAuthClientId(builder.Configuration["Swagger:ClientId"]);
+        setup.OAuthAppName("MattiasTestApp");
+        setup.OAuthScopeSeparator(" ");
+        setup.OAuthUsePkce();
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
