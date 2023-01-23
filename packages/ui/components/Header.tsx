26a0ea4eea732541cdb858994e2652c38f1f4f2d
Simon Johansson <simon.johansson@spinit.se>
2023-01-23 15:31:59 +0100

    Added tailwind plugin

--- packages/ui/components/Header.tsx
+++ packages/ui/components/Header.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 
-
 interface HeaderProps {
-/**
-* Is this the principal call to action on the page?
-*/
-	title: string;
+  /**
+   * Is this the principal call to action on the page?
+   */
+  title: string;
 }
 
 /**
  * The H1 Page header
  */
-export const Header = ({title}: HeaderProps) => {
-	return <h1 className="text-6xl font-semibold">{title}</h1>;
+export const Header = ({ title }: HeaderProps) => {
+  return <h1 className="text-6xl font-semibold ">{title}</h1>;
 };
